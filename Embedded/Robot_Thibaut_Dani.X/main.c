17c7ce799ce3bf0a2737a122c9a82a27620d89d0
juliengld <150852639+juliengld@users.noreply.github.com>
2025-01-06 18:35:30 +0100

    Update main.c

--- Embedded/Robot_Thibaut_Dani.X/main.c
+++ Embedded/Robot_Thibaut_Dani.X/main.c
@@ -76,7 +76,7 @@ int main(void) {
                 LED_VERTE_1 = 1;
             else
                 LED_VERTE_1 = 0;
-        }//eux ils ont Ã©tÃ© mis a jour
+        }//eux ils ont ÃÂ©tÃÂ© mis a jour
     }
 }
 
@@ -93,119 +93,68 @@ void SetNextRobotStateInAutomaticMode() {
     if (robotState.distanceTelemetreDroit < 30) capteurs |= 0b00010; // Bit 1
     if (robotState.distanceTelemetreExtremeDroit < 30) capteurs |= 0b00001; // Bit 0
 
-    // Gestion des Ã©tats en fonction des capteurs
+    // Gestion des ÃÂ©tats en fonction des capteurs
     switch (capteurs) {
             // Aucun obstacle
         case 0b00000:
+        case 0b10001:
             nextStateRobot = STATE_AVANCE;
             break;
 
-            // Obstacle au centre uniquement
+        // Obstacle au centre uniquement
         case 0b00100:
+        case 0b11010:
+        case 0b01011:
             nextStateRobot = STATE_TOURNE_SUR_PLACE;
             break;
-
-            // Obstacles simultanÃ©s Ã   l'Extreme gauche et Ã  l'Extreme droite
-        case 0b10001:
-            nextStateRobot = STATE_AVANCE_PRUDENT;
-            break;
-
-        case 0b11010: // Cas extrÃªme gauche + droite + gauche
-        case 0b10110: // Extreme Gauche + centre + droite
+            
+        case 0b11100:
+        case 0b10100:
+        case 0b01100:        
             nextStateRobot = STATE_TOURNE_SUR_PLACE_DROITE;
             break;
-        case 0b11001: // Partout sauf droite et centre
-        case 0b01000: // Gauche
-            nextStateRobot = STATE_TOURNE_DROITE;
-            break;
-
-        case 0b01011: // Cas Extreme doite + droite + gauche 
-        case 0b01101: // Extreme Droite + centre + gauche
+            
+        case 0b00111:
+        case 0b00101:
+        case 0b00110:
             nextStateRobot = STATE_TOURNE_SUR_PLACE_GAUCHE;
             break;
-        case 0b10011: //Partout sauf Gauche et Centre
+
+        case 0b10000:
+        case 0b01000:
+        case 0b11000:
+        case 0b01001:
+            nextStateRobot = STATE_TOURNE_DROITE;
+            break;
+         
+        case 0b00010:
+        case 0b00001:
+        case 0b00011:
+        case 0b10010:
             nextStateRobot = STATE_TOURNE_GAUCHE;
             break;
             
-
-        case 0b10111: // Partout sauf gauche
-        case 0b01010:// Obstacles simultanÃ©s Ã  gauche et Ã  droite
-        case 0b10101: // Extremes + centre
-            nextStateRobot = STATE_TOURNE_SUR_PLACE_GAUCHE; // Tourne sur place vers la gauche
-            break;
-
-        case 0b11101:
-            nextStateRobot = STATE_TOURNE_SUR_PLACE_DROITE; // Tourne sur place vers la gauche
-            break;
-
-            // Obstacles uniquement sur les extrÃªmes
-        case 0b10000: // ExtrÃªme gauche uniquement
-        case 0b10010: // Extreme gauche + droite
-            nextStateRobot = STATE_TOURNE_SUR_PLACE_DROITE;
-            break;
-
-        case 0b00001: // ExtrÃªme droite uniquement
-        case 0b01001: // Extreme droite + gauche
-            nextStateRobot = STATE_TOURNE_SUR_PLACE_GAUCHE; // ExtrÃªme droite : virage lÃ©ger Ã  gauche
-            break;
-
-            // Obstacle Ã  l'extrÃªme gauche + gauche
-        case 0b11000:
-            nextStateRobot = STATE_TOURNE_DROITE; // Tourne Ã  droite normalement
-            break;
-
-            // Obstacle Ã  l'extrÃªme droite + droite
-        case 0b00011:
-            nextStateRobot = STATE_TOURNE_GAUCHE; // Tourne Ã  gauche normalement
-            break;
-
-            // Obstacles multiples Ã  gauche, incluant le centre
-        case 0b10100:
-        case 0b11100:
-        case 0b01100: // Gauche + Centre
-            nextStateRobot = STATE_TOURNE_DROITE;
-            break;
-
-            // Obstacles multiples Ã  droite, incluant le centre
-        case 0b00110://Droite + Centre
-        case 0b00111: // Partout sauf cotÃ© gauhce
-        case 0b00010: // Droite
-        case 0b00101: // Centre + Droite
-            nextStateRobot = STATE_TOURNE_GAUCHE;
-            break;
-
-            // Obstacles partout sauf Ã  l'extrÃªme gauche
         case 0b01111:
-            nextStateRobot = STATE_TOURNE_SUR_PLACE_GAUCHE; // Tourner vers la droite (espace libre Ã  gauche)
-            break;
-
-            // Obstacles partout sauf Ã  l'extrÃªme droite
+        case 0b10111:
+        case 0b11011:
+        case 0b11101:
         case 0b11110:
-            nextStateRobot = STATE_TOURNE_SUR_PLACE_DROITE; // Tourner vers la gauche (espace libre Ã  droite)
+        case 0b11111:
+        case 0b01110:
+        case 0b10110:
+        case 0b01101:
+        case 0b01010:
+        case 0b10101:
+            nextStateRobot = STATE_RECULE;
             break;
-
-
-        case 0b11111: // Obstacles partout
-        case 0b01110: // Obstacles partout sauf aux extremes
-        case 0b11011: // Obstacles partout sauf au centre
-            nextStateRobot = STATE_TOURNE_SUR_PLACE;
+            
+        default :
+            nextStateRobot = STATE_AVANCE;
             break;
-
-            // Gestion gÃ©nÃ©rique (prioritÃ© au centre, puis gauche, puis droite)
-        default:
-            if (capteurs & 0b00100) {
-                nextStateRobot = STATE_TOURNE_SUR_PLACE; // Centre prioritaire
-            } else if (capteurs & 0b11000) {
-                nextStateRobot = STATE_TOURNE_DROITE; // Virage Ã  droite
-            } else if (capteurs & 0b00011) {
-                nextStateRobot = STATE_TOURNE_GAUCHE; // Virage Ã  gauche
-            } else {
-                nextStateRobot = STATE_STOP; // SÃ©curitÃ© par dÃ©faut
-            }
-            break;
+            
     }
 
-    //Si l’on n’est pas dans la transition de lé’tape en cours
+    //Si lâon nâest pas dans la transition de lÃ©âtape en cours
     if (nextStateRobot != stateRobot - 1)
         stateRobot = nextStateRobot;
 
@@ -248,25 +197,6 @@ void OperatingSystemLoop(void) {
             SetNextRobotStateInAutomaticMode();
             break;
 
-        case STATE_TOURNE_SUR_PLACE_GAUCHE:
-            PWMSetSpeedConsigne(30, MOTEUR_DROIT);
-            PWMSetSpeedConsigne(30, MOTEUR_GAUCHE);
-            stateRobot = STATE_TOURNE_SUR_PLACE_GAUCHE_EN_COURS;
-            break;
-
-        case STATE_TOURNE_SUR_PLACE_GAUCHE_EN_COURS:
-            SetNextRobotStateInAutomaticMode();
-            break;
-
-        case STATE_TOURNE_SUR_PLACE_DROITE:
-            PWMSetSpeedConsigne(-30, MOTEUR_DROIT);
-            PWMSetSpeedConsigne(-30, MOTEUR_GAUCHE);
-            stateRobot = STATE_TOURNE_SUR_PLACE_DROITE_EN_COURS;
-            break;
-
-        case STATE_TOURNE_SUR_PLACE_DROITE_EN_COURS:
-            SetNextRobotStateInAutomaticMode();
-            break;
 
         case STATE_TOURNE_DROITE:
             PWMSetSpeedConsigne(-5, MOTEUR_DROIT);
@@ -288,40 +218,16 @@ void OperatingSystemLoop(void) {
             SetNextRobotStateInAutomaticMode();
             break;
 
-        case STATE_TOURNE_DROITE_LEGER:
-            PWMSetSpeedConsigne(-15, MOTEUR_DROIT);
-            PWMSetSpeedConsigne(30, MOTEUR_GAUCHE);
-            stateRobot = STATE_TOURNE_DROITE_LEGER_EN_COURS;
-            break;
-
-        case STATE_TOURNE_DROITE_LEGER_EN_COURS:
-            SetNextRobotStateInAutomaticMode();
-            break;
-
-        case STATE_TOURNE_GAUCHE_LEGER:
-            PWMSetSpeedConsigne(-30, MOTEUR_DROIT);
-            PWMSetSpeedConsigne(15, MOTEUR_GAUCHE);
-            stateRobot = STATE_TOURNE_GAUCHE_LEGER_EN_COURS;
-            break;
-
-        case STATE_TOURNE_GAUCHE_LEGER_EN_COURS:
-            SetNextRobotStateInAutomaticMode();
-            break;
-
-        case STATE_AVANCE_PRUDENT:
-            PWMSetSpeedConsigne(-10, MOTEUR_DROIT);
-            PWMSetSpeedConsigne(10, MOTEUR_GAUCHE);
-            stateRobot = STATE_AVANCE_PRUDENT_EN_COURS;
-            break;
-
-        case STATE_AVANCE_PRUDENT_EN_COURS:
-            SetNextRobotStateInAutomaticMode();
-            break;
-
         case STATE_RECULE:
-            PWMSetSpeedConsigne(10, MOTEUR_DROIT);
-            PWMSetSpeedConsigne(-10, MOTEUR_GAUCHE);
-            stateRobot = STATE_RECULE_EN_COURS;
+            PWMSetSpeedConsigne(15, MOTEUR_DROIT);
+            PWMSetSpeedConsigne(-15, MOTEUR_GAUCHE);
+            if (!(robotState.distanceTelemetreExtremeGauche < 30 ||
+                  robotState.distanceTelemetreGauche < 30 ||
+                  robotState.distanceTelemetreCentre < 30 ||
+                  robotState.distanceTelemetreDroit < 30 ||
+                  robotState.distanceTelemetreExtremeDroit < 30)) {
+                stateRobot = STATE_TOURNE_SUR_PLACE;
+            }
             break;
 
         case STATE_RECULE_EN_COURS:
@@ -329,10 +235,9 @@ void OperatingSystemLoop(void) {
             break;
 
         default:
-            stateRobot = STATE_ATTENTE;
+            stateRobot = STATE_AVANCE;
             break;
     }
 }
-
 
 
